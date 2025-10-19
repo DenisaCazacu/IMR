cd07e1e1fc67d7e7de61855c88ac0b5b91382ceb
DenisaCazacu <denisa.cazacu01@gmail.com>
2025-10-19 21:43:11 +0300

    Update XR Controller and initiate homework 3

---
+++ Tema2/Assets/GrabDetector.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class GrabDetector : MonoBehaviour
+{
+    private XRDirectInteractor directInteractor;
+
+    void Start()
+    {
+        directInteractor = GetComponent<XRDirectInteractor>();
+
+        if (directInteractor != null)
+        {
+            Debug.Log(" GrabDetector: XRDirectInteractor gasit cu succes.");
+            directInteractor.selectEntered.AddListener(OnGrabStart);
+            directInteractor.selectExited.AddListener(OnGrabEnd);
+        }
+        else
+        {
+            Debug.LogWarning(" GrabDetector: Nu s-a gasit un XRDirectInteractor pe acest obiect!");
+        }
+
+        Debug.Log(" GrabDetector: Scriptul a fost initializat cu succes.");
+    }
+
+    private void OnGrabStart(SelectEnterEventArgs args)
+    {
+        Debug.Log(" GrabDetector: OnGrabStart apelat.");
+        GameObject grabbedObject = args.interactableObject.transform.gameObject;
+        Debug.Log($"Grab START → Ai apucat: {grabbedObject.name}");
+    }
+
+    private void OnGrabEnd(SelectExitEventArgs args)
+    {
+        GameObject releasedObject = args.interactableObject.transform.gameObject;
+        Debug.Log($"Grab END → Ai eliberat: {releasedObject.name}");
+    }
+}
